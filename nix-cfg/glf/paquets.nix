24d8936bfe4a5cde059d4f8f323811742f49e87f
NemNix <184756415+NemNix@users.noreply.github.com>
2024-12-10 17:48:58 +0100

    Iso Name

--- nix-cfg/glf/paquets.nix
+++ nix-cfg/glf/paquets.nix
@@ -4,9 +4,7 @@
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install -y flathub org.dupot.easyflatpak
-    '';
+    script = '' flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install -y flathub org.dupot.easyflatpak '';
   };
 
   environment.systemPackages = with pkgs;[
@@ -14,7 +12,7 @@
     discord
 
     libreoffice
-    # hunspell
-    # hunspellDicts.fr-moderne
+    hunspell
+    hunspellDicts.fr-moderne
   ];
 } 
